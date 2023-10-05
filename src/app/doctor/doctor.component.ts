8d3136b7c22e17070056ff29f8804c7db9569006
SivaBharat <sivabharthece@gmail.com>
2023-10-05 13:04:06 +0530

    imgg

--- src/app/doctor/doctor.component.ts
+++ src/app/doctor/doctor.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import{environment}from 'src/environment/environments';
 @Component({
   selector: 'app-doctor',
   templateUrl: './doctor.component.html',
   styleUrls: ['./doctor.component.css']
 })
-export class DoctorComponent {
+export class DoctorComponent implements OnInit {
+  doctors: any[] = [];
+  departments:any[]=[];
+  constructor(private http: HttpClient) { }
 
+  ngOnInit(): void {    
+    this.http.get(environment.doctor).subscribe((data: any) => {
+      this.doctors = data;
+    });
+    this.http.get('https://localhost:44324/api/Departments').subscribe((data: any) => {
+      this.departments = data;
+    });
+  }
+  getDepartmentName(deptId: number): string {
+    const department = this.departments.find((dept) => dept.deptId === deptId);
+    return department ? department.deptName : 'Unknown'; 
+  }
+  
 }
--- src/app/staff/staff.component.ts
+++ src/app/staff/staff.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import{environment}from 'src/environment/environments';
 @Component({
   selector: 'app-staff',
   templateUrl: './staff.component.html',
   styleUrls: ['./staff.component.css']
 })
-export class StaffComponent {
+export class StaffComponent implements OnInit {
+  staffs: any[] = [];
+  departments:any[]=[];
+  constructor(private http: HttpClient) { }
 
+  ngOnInit(): void {    
+    this.http.get(environment.staff).subscribe((data: any) => {
+      this.staffs = data;
+    });
+    this.http.get('https://localhost:44324/api/Departments').subscribe((data: any) => {
+      this.departments = data;
+    });
+  }
+  getDepartmentName(deptId: number): string {
+    const department = this.departments.find((dept) => dept.deptId === deptId);
+    return department ? department.deptName : 'Unknown'; 
+  }
+  
 }
